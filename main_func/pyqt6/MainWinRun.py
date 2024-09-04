4c7e7d8081aa94b0ce39daa375175178884b2625
yanxiang12138 <s1092952@stud.sbg.ac.at>
2024-09-04 23:56:44 +0800

    尝试pyqt6

---
+++ main_func/pyqt6/MainWinRun.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from untitled import *
+
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    my_win = MyMainWindow()
+    my_win.show()
+    sys.exit(app.exec())
