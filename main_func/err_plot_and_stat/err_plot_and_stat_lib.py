b37c37768914808eea188e52483e687c475a04ff
awanoob <jc77593320@outlook.com>
2024-09-11 19:19:17 +0800

    误差计算与统计函数修改
    调整误差计算与统计函数架构

--- main_func/err_plot_and_stat/err_plot_and_stat_lib.py
+++ main_func/err_plot_and_stat/err_plot_and_stat_lib.py
@@ -1,7 +1,6 @@
 import matplotlib
 import matplotlib.pyplot as plt
-from os.path import join, exists
-from os import makedirs
+from os.path import join
 import numpy as np
 
 
@@ -38,17 +37,12 @@ def fig_plt(xaxis: np.ndarray, yaxis_list, title: list, xlabel: str, ylabel: str
 
 
 def err_time_plot_stat(errlist_scn, path_scene, scene_name, input_cfg) -> np.ndarray:
-    if input_cfg['output_fig']:
-        # 生成场景子路径
-        path_fig_stat = join(path_scene, 'fig_stat')
-        if not exists(path_fig_stat):
-            makedirs(path_fig_stat)
-
+    if input_cfg['output_fig']: 
         # 生成场景误差时间图像
-        fig_xy_path = join(path_fig_stat, f'{scene_name}_xy.png')
-        fig_pos_alt_path = join(path_fig_stat, f'{scene_name}_pos_alt.png')
-        fig_vel_path = join(path_fig_stat, f'{scene_name}_vel.png')
-        fig_att_path = join(path_fig_stat, f'{scene_name}_att.png')
+        fig_xy_path = join(path_scene, f'{scene_name}_xy.png')
+        fig_pos_alt_path = join(path_scene, f'{scene_name}_pos_alt.png')
+        fig_vel_path = join(path_scene, f'{scene_name}_vel.png')
+        fig_att_path = join(path_scene, f'{scene_name}_att.png')
         fig_plt(errlist_scn[:, [1]], [errlist_scn[:, [2]], errlist_scn[:, [3]]], ['横向误差', '纵向误差'], 'Time/s', '/m', fig_xy_path)
         fig_plt(errlist_scn[:, [1]], [errlist_scn[:, [11]], errlist_scn[:, [4]]], ['水平误差', '高程误差'], 'Time/s', '/m', fig_pos_alt_path)
         fig_plt(errlist_scn[:, [1]], [errlist_scn[:, [5]], errlist_scn[:, [6]], errlist_scn[:, [7]]], ['ve误差', 'vn误差', 'vu误差'], 'Time/s', '/(m/s)', fig_vel_path)
@@ -116,10 +110,6 @@ def err_time_plot_stat(errlist_scn, path_scene, scene_name, input_cfg) -> np.nda
     return L_scn
 
 
-def fix_float_err_time_plot_stat():
-    pass
-
-
 def dr_err_stat():
     pass
 
@@ -128,4 +118,4 @@ def multi_dev_err_plot():
     pass
 
 
-__all__ = ['err_time_plot_stat', 'fix_float_err_time_plot_stat', 'dr_err_stat', 'multi_dev_err_plot']
+__all__ = ['err_time_plot_stat', 'dr_err_stat', 'multi_dev_err_plot']
--- main_func/err_plot_and_stat/result_gen.py
+++ main_func/err_plot_and_stat/result_gen.py
@@ -8,6 +8,8 @@ import pandas as pd
 
 def result_gen_func(array_bchmk: np.ndarray, array_test: np.ndarray, errlist: np.ndarray, path_main: str, input_cfg: dict):
     L_all = np.empty((0, 14))
+    L_fix_all = np.empty((0, 14))
+    L_float_all = np.empty((0, 14))
     dev_name = path_main.split('\\')[-1]
     for i in range(len(input_cfg['era_list']['Scene'])):
         # 获取场景名称和时间段
@@ -38,11 +40,28 @@ def result_gen_func(array_bchmk: np.ndarray, array_test: np.ndarray, errlist: np
         output_navplot(array_test_scn, join(path_scene, f"{scene_name}_test.navplot"))
 
         # 输出单场景误差时间图像和统计报告
-        L_scn = err_time_plot_stat(errlist_scn, path_scene, scene_name, input_cfg)
+        path_fig_stat = join(path_scene, 'fig_stat')
+        if not exists(path_fig_stat):
+            makedirs(path_fig_stat)
+        L_scn = err_time_plot_stat(errlist_scn, path_fig_stat, scene_name, input_cfg)
         L_all = np.vstack((L_all, L_scn))
 
         # 输出单场景固定解，浮动解误差时间图像和统计报告
-        fix_float_err_time_plot_stat(errlist_scn, path_scene, scene_name, input_cfg)
+        if errlist_scn[:, [13]].all() == 1 or np.sum(errlist_scn[:, [13]] == 6) / len(errlist_scn) > 0.8:
+            L_fix_scn = np.empty((0, 14))
+            L_fix_all = np.vstack((L_fix_all, L_fix_scn))
+            L_float_scn = np.empty((0, 14))
+            L_float_all = np.vstack((L_float_all, L_float_scn))
+        else:
+            path_ff_fig_stat = join(path_scene, 'ff_fig_stat')
+            if not exists(path_ff_fig_stat):
+                makedirs(path_ff_fig_stat)
+            errlist_fix_scn = errlist_scn[np.where(errlist_scn[:, [13]] == 1)[0]]
+            errlist_float_scn = errlist_scn[np.where(errlist_scn[:, [13]] == 2)[0]]
+            L_fix_scn = err_time_plot_stat(errlist_fix_scn, path_ff_fig_stat, f'{scene_name}_fix', input_cfg)
+            L_float_scn = err_time_plot_stat(errlist_float_scn, path_ff_fig_stat, f'{scene_name}_float', input_cfg)
+            L_fix_all = np.vstack((L_fix_all, L_fix_scn))
+            L_float_all = np.vstack((L_float_all, L_float_scn))    
 
         # 判断是否为隧道场景，并输出单场景DR误差统计，隧道场景特征为测试数据的搜星数为0的占比大于0.75
         # 如果没有搜星数这个字段咋办？
@@ -50,10 +69,14 @@ def result_gen_func(array_bchmk: np.ndarray, array_test: np.ndarray, errlist: np
 
             errlist_scn = errlist[np.where((array_bchmk[:, [1]] >= start_t) & (array_bchmk[:, [1]] < end_t + 30))[0]]
             dr_err_stat(errlist_scn, path_scene, scene_name, input_cfg)
-    
+
     # 将ndarray转换为DataFrame
     L_all = pd.DataFrame(L_all)
+    L_fix_all = pd.DataFrame(L_fix_all)
+    L_float_all = pd.DataFrame(L_float_all)
 
 
     # 输出统计表
     L_all.to_csv(join(path_main, f'{dev_name}_误差统计表.csv'), index=False)
+    L_fix_all.to_csv(join(path_main, f'{dev_name}_固定解误差统计表.csv'), index=False)
+    L_float_all.to_csv(join(path_main, f'{dev_name}_浮动解误差统计表.csv'), index=False)
