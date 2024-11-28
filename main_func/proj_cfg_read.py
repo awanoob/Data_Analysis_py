940426715812e72ad827afc1d3e88fc5a68ef0ee
awanoob <jc77593320@outlook.com>
2024-11-28 16:29:37 +0800

    清理文件目录

--- main_func/proj_cfg_read.py
+++
@@ -1,8 +0,0 @@
-import yaml as ym
-from yaml import CLoader
-
-
-def yaml_read(yaml_path):
-    with open(yaml_path, 'r', encoding='UTF-8') as yaml_file_in:
-        input_cfg = ym.load(yaml_file_in, Loader=CLoader)
-    return input_cfg
